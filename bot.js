1dcb2e506a605873ded277d41f238055ecbdf43b
Kamronbek <kmamadaliyev05@gmail.com>
2026-05-08 00:32:12 +0500

    Set up Telegraf bot with session handling
    Initialize Telegraf bot with session management and admin ID.

--- bot.js
+++ bot.js
@@ -1,1 +1,16 @@
+const { Telegraf, Markup, session } = require('telegraf');
+const { User, Config } = require('./models');
 
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const ADMIN_ID = 6137845806;
+
+bot.use(session());
+bot.use((ctx, next) => {
+    if (!ctx.session) ctx.session = {};
+    return next();
+});
+
+// Yordamchi funksiyalar (checkSub, getMainMenu va h.k. yuqoridagi koddan olinadi)
+// Barcha bot.start, bot.action, bot.on funksiyalarini shu yerga joylang.
+
+module.exports = bot;
