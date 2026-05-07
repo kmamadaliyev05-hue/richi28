e891750d3390ec53461abc7548fbbd62360d0395
Kamronbek <kmamadaliyev05@gmail.com>
2026-05-07 19:47:44 +0500

    Update models.js

--- models.js
+++ models.js
@@ -1,13 +1,1 @@
-const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    userId: { type: Number, unique: true, required: true },
-    firstName: String,
-    username: String,
-    status: { type: String, default: 'new' }, // 'new', 'requested', 'pending_id'
-    gameId: { type: String, default: null },
-    isVerified: { type: Boolean, default: false },
-    joinedAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('User', userSchema);
