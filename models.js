895dac9e7fbe95a6be194968a54199eca9853eb4
Kamronbek <kmamadaliyev05@gmail.com>
2026-05-08 00:35:16 +0500

    Update models.js

--- models.js
+++ models.js
@@ -4,7 +4,9 @@ const userSchema = new mongoose.Schema({
     userId: { type: Number, unique: true },
     firstName: String,
     username: String,
-    status: { type: String, default: 'new' },
+    status: { type: String, default: 'new' }, // new, requested, id_submitted, verified
+    bookmaker: String,
+    gameId: String,
     isVerified: { type: Boolean, default: false },
     referralCount: { type: Number, default: 0 },
     refTask: { type: Number, default: 5 },
@@ -13,9 +15,9 @@ const userSchema = new mongoose.Schema({
 
 const configSchema = new mongoose.Schema({
     key: { type: String, unique: true },
-    value: String,
-    chatId: String,
-    url: String
+    value: String, // Kanal nomi
+    chatId: String, // -100...
+    url: String // https://t.me/...
 });
 
 const User = mongoose.model('User', userSchema);
